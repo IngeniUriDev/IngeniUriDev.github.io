7061554cc417fc2b8d6d18e913dd132df63a6560
Uriel Rojas Gómez <IngeniUriDev@users.noreply.github.com>
2026-06-08 16:14:37 -0600

    Add CRUD frontend and fix API URL

--- backend/db.py
+++ backend/db.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Render inyectará DATABASE_URL como variable de entorno
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://root:zn8Wzoq0m8iQJvHBc1CsKsASiRkyvwVj@dpg-d8jj804m0tmc73fch5sg-a.oregon-postgres.render.com/cv_prueba")
 
 engine = create_engine(DATABASE_URL, future=True, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
